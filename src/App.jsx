5befbb80975b55ec53517562ce833f10e0097b05
nikhilsoni7309 <113381403+nikhilsoni7309@users.noreply.github.com>
2024-04-06 15:53:04 +0530

    styling navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
+
+import Navbar from './components/Navbar'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Portfolio from './pages/portfolio/Portfolio'
+import Contact from './pages/contact/Contact'
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
